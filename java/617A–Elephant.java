10d6c197bade393071ffe6cb5ec8766df6044d3f
aamahi <amahi.wp@gmail.com>
2021-09-27 15:24:32 +0600

    617A–Elephant.java

---
+++ java/617A–Elephant.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Elephant {
+ 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        System.out.println((x + 4) / 5);
+    }
+}
